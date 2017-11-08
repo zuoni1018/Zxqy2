cf98de6e175f95a657a89fc0970d15085f5074fc
zuoni <767450430@qq.com>
2017-11-08 10:37:36 +0800

    删除一些没用的资源

---
+++ app/src/main/java/com/zuoni/zxqy/ui/fragment/base/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.zuoni.zxqy.ui.fragment.base;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by zangyi_shuai_ge on 2017/11/8
+ */
+
+public class BaseFragment extends Fragment {
+}
